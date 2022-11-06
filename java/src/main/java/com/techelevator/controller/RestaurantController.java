bed5dc7f354f21c8d0d27a3a7b1ce5a88d0de3ea
Kevin Booms <mrkbooms@gmail.com>
2022-11-05 21:12:18 -0400

    I hope I pushed the right one this time.

--- java/src/main/java/com/techelevator/controller/RestaurantController.java
+++ java/src/main/java/com/techelevator/controller/RestaurantController.java
@@ -5,9 +5,11 @@ import com.techelevator.model.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
 
