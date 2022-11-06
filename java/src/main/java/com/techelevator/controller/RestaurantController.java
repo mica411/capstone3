6f6a3d377416a889afeea34ae720354b8e542873
Kevin Booms <mrkbooms@gmail.com>
2022-11-05 20:42:21 -0400

    Restaurant Controller still testing 404 in Postman

--- java/src/main/java/com/techelevator/controller/RestaurantController.java
+++ java/src/main/java/com/techelevator/controller/RestaurantController.java
@@ -1,12 +1,28 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.RestaurantDao;
+import com.techelevator.model.Restaurant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
 
-    private RestaurantDao restaurantDao;
+    List<Restaurant> restaurants;
 
+    @Autowired
+    RestaurantDao restaurantDao;
 
+    public RestaurantController(RestaurantDao restaurantDao) {
+        this.restaurantDao = restaurantDao;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Restaurant> findAll() {
+        return restaurants;
+    }
 }
+
